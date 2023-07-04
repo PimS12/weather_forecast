44b9fa4a0eff723000bf14008109967dc7d95074
PimS12 <apimenoffs@gmail.com>
2023-07-04 13:07:20 +0300

    application layout

---
+++ src/components/WeatherDaily.vue
@@ -0,0 +1,165 @@
+<template>
+    <div class="weather__container">
+      <div class="weather__wrapper">
+        <div class="weather__title">По дням</div>
+        
+        <div class="weather__block-tape">
+
+          <div class="weather__block-tape__item">
+            <div class="weather__day">Понедельник</div>
+            
+            <div class="weather__icon"><img src="@/assets/sun.svg" alt=""></div>
+
+            <div class="weather__temp">
+              <div class="weather__temp-max">4°</div>
+              <div class="weather__temp-min">3°</div>            
+            </div>
+          </div>
+
+          <div class="weather__block-tape__item">
+            <div class="weather__day">Понедельник</div>
+            
+            <div class="weather__icon"><img src="@/assets/sun.svg" alt=""></div>
+
+            <div class="weather__temp">
+              <div class="weather__temp-max">4°</div>
+              <div class="weather__temp-min">3°</div>            
+            </div>
+          </div>
+
+          <div class="weather__block-tape__item">
+            <div class="weather__day">Понедельник</div>
+            
+            <div class="weather__icon"><img src="@/assets/sun.svg" alt=""></div>
+
+            <div class="weather__temp">
+              <div class="weather__temp-max">4°</div>
+              <div class="weather__temp-min">3°</div>            
+            </div>
+          </div>
+
+          <div class="weather__block-tape__item">
+            <div class="weather__day">Понедельник</div>
+            
+            <div class="weather__icon"><img src="@/assets/sun.svg" alt=""></div>
+
+            <div class="weather__temp">
+              <div class="weather__temp-max">4°</div>
+              <div class="weather__temp-min">3°</div>            
+            </div>
+          </div>
+
+          <div class="weather__block-tape__item">
+            <div class="weather__day">Понедельник</div>
+            
+            <div class="weather__icon"><img src="@/assets/sun.svg" alt=""></div>
+
+            <div class="weather__temp">
+              <div class="weather__temp-max">4°</div>
+              <div class="weather__temp-min">3°</div>            
+            </div>
+          </div>
+
+          <div class="weather__block-tape__item">
+            <div class="weather__day">Понедельник</div>
+            
+            <div class="weather__icon"><img src="@/assets/sun.svg" alt=""></div>
+
+            <div class="weather__temp">
+              <div class="weather__temp-max">4°</div>
+              <div class="weather__temp-min">3°</div>            
+            </div>
+          </div>
+
+          <div class="weather__block-tape__item">
+            <div class="weather__day">Понедельник</div>
+            
+            <div class="weather__icon"><img src="@/assets/sun.svg" alt=""></div>
+
+            <div class="weather__temp">
+              <div class="weather__temp-max">4°</div>
+              <div class="weather__temp-min">3°</div>            
+            </div>
+          </div>
+
+          <!-- Остальные блоки с классом "weather__block-tape__item" -->
+
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+    .weather__wrapper {
+    padding-top: 81px;
+    }
+
+    .weather__title {
+    color: #FFF;
+    font-size: 24px;
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 135%;
+
+    margin-bottom: 8px;
+    }
+
+    .weather__block-tape {
+    display: inline-flex;
+    align-items: flex-start;
+    gap: 32px;
+
+    padding-top: 33px;
+    }
+
+    .weather__block-tape__item {
+    display: flex;
+    width: 160px;
+    padding: 16px;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 16px;
+    }
+
+    .weather__block-tape__item:hover {
+    border-radius: 16px;
+    background: rgba(255, 255, 255, 0.25);
+    }
+
+    .weather__block-tape__item .weather__icon {
+    width: 48px;
+    height: 48px;
+    }
+
+    .weather__temp {
+    display: flex;
+    gap: 16px;
+    }
+
+    .weather__temp-max {
+    color: #FFF;
+    font-size: 24px;
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 135%;
+    }
+
+    .weather__temp-min {
+    display: flex;
+    align-items: flex-end;
+    color: #FFF;
+    font-size: 18px;
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 135%;
+    
+    opacity: 0.5;
+    }
+</style>
---
+++ src/components/WeatherMain.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="weather__container">
+      <div class="weather__block-main">
+        <div class="weather__city">Москва</div>
+
+        <div class="weather__info">
+          <div class="weather__icon"><img src="@/assets/sun.svg" alt=""></div>
+          <div class="weather__temp-now">--°</div>        
+        </div>
+
+        <div class="weather__status">Солнечно</div>
+      </div>    
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+    .weather__block-main {
+    padding-top: 64px;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    }
+
+    .weather__info {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    gap: 24px;
+    }
+
+    .weather__city {
+    color: #FFF;
+    font-size: 36px;
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 130%;
+    }
+
+    .weather__temp-now {
+    color: #FFF;
+    font-size: 96px;
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: 300;
+    line-height: 130%;
+    }
+
+    .weather__status {
+    color: #FFF;
+    font-size: 24px;
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 130%;
+    }
+</style>
